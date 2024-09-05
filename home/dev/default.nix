795a69c7c1b96ab6a6dd3f52559e007d7d3d28a7
Lemonife <lemonife@protonmail.com>
2024-09-05 14:39:51 -0400

    feat: config cpp environment

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -3,5 +3,9 @@
   home.packages = with pkgs; [
     (rust-bin.stable.latest.default.override { extensions = [ "rust-analyzer" ]; })
     python313Full
+    cmake
+    clang
+    clang-tools
+    bear
   ];
 }
