35cce058cdb52bb4f63933413fae5436b58b466f
Lemonife <lemonife@protonmail.com>
2024-09-07 22:46:32 -0400

    feat(yazi): add tokyonight tmTheme

--- home/shell/cli.nix
+++ home/shell/cli.nix
@@ -1,14 +1,18 @@
 { lib, inputs, ... }:
 {
+  xdg.configFile."yazi/tokyonight_moon.tmTheme".source = "${inputs.tokyonight}/extras/sublime/tokyonight_moon.tmTheme";
   programs.yazi = {
     enable = true;
-    theme = lib.importTOML "${inputs.tokyonight}/extras/yazi/tokyonight_moon.toml";
+    theme = lib.mkMerge [
+      (lib.importTOML "${inputs.tokyonight}/extras/yazi/tokyonight_moon.toml")
+      { highlight = "./tokyonight_moon.tmTheme"; }
+    ];
+    enableFishIntegration = true;
   };
+
   programs.ripgrep.enable = true;
   programs.fd.enable = true;
-
-  programs.fzf = {
-    enable = true;
-  };
+  programs.fzf.enable = true;
+  # Set fzf theme
   programs.fish.interactiveShellInit = builtins.readFile "${inputs.tokyonight}/extras/fzf/tokyonight_moon.sh";
 }
