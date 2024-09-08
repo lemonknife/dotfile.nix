bd22c6797712f8693723feabc6627f220644f36e
Lemonife <lemonife@protonmail.com>
2024-09-08 17:32:51 -0400

    feat: add clipboard

--- host/desktop-environment.nix
+++ host/desktop-environment.nix
@@ -1,7 +1,11 @@
+{ pkgs, ... }:
 {
   # Display Manager (cosmic)
   services.displayManager.cosmic-greeter.enable = true;
 
   # Cosmic
   services.desktopManager.cosmic.enable = true;
+
+  # Clipboard Support
+  environment.systemPackages = with pkgs; [ wl-clipboard ];
 }
