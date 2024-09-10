b3981346281a1a6b47d1c713edbae4ef2bc6ee88
Lemonife <lemonife@protonmail.com>
2024-09-10 00:11:07 -0400

    feat(nvim): switch to astronvim

---
+++ home/shell/neovim/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "cpp",
+      "python",
+      "rust",
+    },
+  },
+}
