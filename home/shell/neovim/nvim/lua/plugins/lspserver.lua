676c6bbac7630bf48d11fb0c76e450f1ecdede67
Lemonife <lemonife@protonmail.com>
2024-09-10 01:19:26 -0400

    feat(nvim): edit clangd settings

--- home/shell/neovim/nvim/lua/plugins/lspserver.lua
+++ home/shell/neovim/nvim/lua/plugins/lspserver.lua
@@ -4,9 +4,8 @@ return {
   opts = {
     config = {
       lua_ls = { settings = { Lua = { hint = { enable = true, arrayIndex = "Disable" } } } },
-      clangd = { cmd = { "clangd", "--clang-tidy" } },
+      clangd = { cmd = { "clangd", "--clang-tidy" }, capabilities = { offsetEncoding = "utf-8" } },
       basedpyright = { settings = { disableOrganizeImports = true, analysis = { ignore = { "*" } } } },
     },
   },
-
 }
