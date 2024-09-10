0e8099fca4af991beb8553ca49201092e4419bd4
Lemonife <lemonife@protonmail.com>
2024-09-10 00:14:26 -0400

    feat(nvim): add theme

---
+++ home/shell/neovim/nvim/lua/plugins/ui.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    specs = {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = {
+        colorscheme = "tokyonight-moon",
+      },
+    },
+  },
+}
