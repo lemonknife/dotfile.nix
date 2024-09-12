a64c05699839e096d126723779ccdae6a5f32e7a
Lemonife <lemonife@protonmail.com>
2024-09-12 01:16:33 -0400

    feat(nvim): edit python formatter

--- home/shell/neovim/nvim/lua/plugins/python.lua
+++ home/shell/neovim/nvim/lua/plugins/python.lua
@@ -32,4 +32,14 @@ return {
       },
     },
   },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", "ruff_format" },
+      },
+    },
+  },
 }
