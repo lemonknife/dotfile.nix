928fc3824fe0544ac7dc75e01146430bafba8e44
Lemonife <lemonife@protonmail.com>
2024-09-12 00:10:54 -0400

    feat: enable ports

--- host/default.nix
+++ host/default.nix
@@ -43,6 +43,11 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 53317 ];
+    allowedUDPPorts = [ 53317 ];
+  };
 
   # Set your time zone.
 
