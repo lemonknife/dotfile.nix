71d5652132f16d1ae0e2e0b29395e2e75663f774
Lemonife <lemonife@protonmail.com>
2024-09-12 23:24:19 -0400

    feat(nvim): edit nvim-cmp ui

---
+++ home/shell/neovim/nvim/lua/plugins/ui.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local colors = require("tokyonight.colors").setup()
+      opts.window = {
+        completion = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:NormalBorder,CursorLine:PmenuSel,Search:None",
+          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        }),
+      }
+
+      vim.api.nvim_set_hl(0, "NormalBorder", { fg = colors.border_highlight, bg = nil })
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
