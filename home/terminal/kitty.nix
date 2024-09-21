1fc77d11b9361c7864dd4323b557285ab7ce24be
Lemonife <lemonife@protonmail.com>
2024-09-21 14:32:14 -0400

    feat(kitty): edit ui

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -11,6 +11,11 @@
       font_features = "CaskaydiaCoveNF-Italic +ss01";
       # Edit background color in case kitty render neovim as transparent background 
       background = "#222437";
+      window_padding_width = "8";
+      hide_window_decorations = "yes";
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
     };
     theme = "Tokyo Night Moon";
   };
