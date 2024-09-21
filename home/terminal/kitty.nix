dd8263459a64b6f6a326849fa47c5dfdbca6209c
Lemonife <lemonife@protonmail.com>
2024-09-21 13:01:18 -0400

    feat: edit kitty setting

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -3,8 +3,12 @@
   programs.kitty = {
     enable = true;
     font = {
-      name = "Cascadia Code PL";
+      name = "CaskaydiaCove Nerd Font";
       size = 15;
+    };
+    settings = {
+      background_opacity = "0.9";
+      font_features = "CaskaydiaCoveNF-Italic +ss01";
     };
     theme = "Tokyo Night Moon";
   };
