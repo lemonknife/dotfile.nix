2328ba1ea597fdb5743616e3ef800491b362713a
Lemonife <lemonife@protonmail.com>
2024-09-21 13:18:26 -0400

    fix(kitty): avoid render neovim background as transparent

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -9,6 +9,8 @@
     settings = {
       background_opacity = "0.9";
       font_features = "CaskaydiaCoveNF-Italic +ss01";
+      # Edit background color in case kitty render neovim as transparent background 
+      background = "#222437";
     };
     theme = "Tokyo Night Moon";
   };
