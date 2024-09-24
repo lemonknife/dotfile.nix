b15087676c994ddf77c8b8723684322233c5e45d
lemonknife <lemonife@protonmail.com>
2024-09-24 01:04:23 -0400

    feat(nvim): add lualine config from LazyVim

--- home/shell/neovim/lmnvim/lua/plugins/ui.lua
+++ home/shell/neovim/lmnvim/lua/plugins/ui.lua
@@ -11,4 +11,56 @@ return {
 		"folke/tokyonight.nvim",
 		opts = { style = "moon" },
 	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			local lualine_utils = require("utils.lualine")
+			-- PERF: we don't need this lualine require madness 🤷
+			local lualine_require = require("lualine_require")
+			lualine_require.require = require
+
+			vim.o.laststatus = vim.g.lualine_laststatus
+
+			local opts = {
+				options = {
+					theme = "auto",
+					globalstatus = vim.o.laststatus == 3,
+					disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+
+					lualine_c = {
+						lualine_utils.root_dir(),
+						{
+							"diagnostics",
+							symbols = {
+								error = " ",
+								warn = " ",
+								hint = " ",
+								info = " ",
+							},
+						},
+						{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+						{ lualine_utils.pretty_path() },
+					},
+				},
+				extensions = { "mason", "lazy" },
+			}
+			return opts
+		end,
+	},
 }
