68ab18e8275ba5163f39fda0961548539be88ad4
lemonknife <lemonife@protonmail.com>
2024-09-27 17:48:48 -0400

    feat(nvim): edit keymaps

--- home/shell/neovim/lmnvim/lua/mappings/init.lua
+++ home/shell/neovim/lmnvim/lua/mappings/init.lua
@@ -1,3 +1,41 @@
-local map = vim.keymap.set
+local map = LazyVim.safe_keymap_set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+map("n", "<leader>gg", function()
+	LazyVim.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Lazygit (Root Dir)" })
+map("n", "<leader>gG", function()
+	LazyVim.lazygit()
+end, { desc = "Lazygit (cwd)" })
+map("n", "<leader>gb", LazyVim.lazygit.blame_line, { desc = "Git Blame Line" })
+map("n", "<leader>gB", LazyVim.lazygit.browse, { desc = "Git Browse" })
+
+map("n", "<leader>gf", function()
+	local git_path = vim.api.nvim_buf_get_name(0)
+	LazyVim.lazygit({ args = { "-f", vim.trim(git_path) } })
+end, { desc = "Lazygit Current File History" })
+
+map("n", "<leader>gl", function()
+	LazyVim.lazygit({ args = { "log" }, cwd = LazyVim.root.git() })
+end, { desc = "Lazygit Log" })
+map("n", "<leader>gL", function()
+	LazyVim.lazygit({ args = { "log" } })
+end, { desc = "Lazygit Log (cwd)" })
