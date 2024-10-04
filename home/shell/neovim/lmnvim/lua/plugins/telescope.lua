1375a3c65735824e654ede1c22ad68f7432558cd
lemonknife <lemonife@protonmail.com>
2024-10-04 15:34:22 -0400

    refactor(nvim): edit telescope file structure

--- home/shell/neovim/lmnvim/lua/plugins/telescope.lua
+++ home/shell/neovim/lmnvim/lua/plugins/telescope.lua
@@ -44,7 +44,47 @@ end
 return {
 	"nvim-telescope/telescope.nvim",
 	cmd = "Telescope",
-	opts = require("options.telescope"),
+	opts = function()
+		local function find_command()
+			return { "rg", "--files", "--color", "never", "-g", "!.git" }
+		end
+
+		local actions = require("telescope.actions")
+
+		return {
+			defaults = {
+				prompt_prefix = " ",
+				selection_caret = " ",
+				-- open files in the first window that is an actual file.
+				-- use the current window if no other window is available.
+				get_selection_window = function()
+					local wins = vim.api.nvim_list_wins()
+					table.insert(wins, 1, vim.api.nvim_get_current_win())
+					for _, win in ipairs(wins) do
+						local buf = vim.api.nvim_win_get_buf(win)
+						if vim.bo[buf].buftype == "" then
+							return win
+						end
+					end
+					return 0
+				end,
+				mappings = {
+					n = {
+						["q"] = actions.close,
+						["<esc>"] = false,
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = find_command,
+					hidden = true,
+				},
+			},
+		}
+	end,
+
+	keys = require("mappings.telescope"),
 	dependencies = {
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
