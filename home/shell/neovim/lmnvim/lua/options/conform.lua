218e6b09c151df754d7054ba0c224f5f789a55d5
lemonknife <lemonife@protonmail.com>
2024-10-05 19:01:52 -0400

    feat(nvim): edit stylua format type

--- home/shell/neovim/lmnvim/lua/options/conform.lua
+++ home/shell/neovim/lmnvim/lua/options/conform.lua
@@ -1,36 +1,42 @@
 vim.api.nvim_create_user_command("FormatDisable", function(args)
-	if args.bang then
-		-- FormatDisable! will disable formatting just for this buffer
-		vim.b.disable_autoformat = true
-	else
-		vim.g.disable_autoformat = true
-	end
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
 end, {
-	desc = "Disable autoformat-on-save",
-	bang = true,
+  desc = "Disable autoformat-on-save",
+  bang = true,
 })
 vim.api.nvim_create_user_command("FormatEnable", function()
-	vim.b.disable_autoformat = false
-	vim.g.disable_autoformat = false
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
 end, {
-	desc = "Re-enable autoformat-on-save",
+  desc = "Re-enable autoformat-on-save",
 })
 
 local options = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		cpp = { "clang-format" },
-		c = { "clang-format" },
-		nix = { "nixfmt" },
-	},
+  formatters = {
+    stylua = {
+      prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+    },
+  },
 
-	format_on_save = function(bufnr)
-		-- Disable with a global or buffer-local variable
-		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-			return
-		end
-		return { timeout_ms = 500, lsp_format = "fallback" }
-	end,
+  formatters_by_ft = {
+    lua = { "stylua" },
+    cpp = { "clang-format" },
+    c = { "clang-format" },
+    nix = { "nixfmt" },
+  },
+
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = "fallback" }
+  end,
 }
 
 return options
