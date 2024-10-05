2553f7fe0584df550990634528befd48727e035e
lemonknife <lemonife@protonmail.com>
2024-10-05 17:35:28 -0400

    fix(nvim): remove comments

--- home/shell/neovim/lmnvim/lua/plugins/tool.lua
+++ home/shell/neovim/lmnvim/lua/plugins/tool.lua
@@ -61,14 +61,13 @@ return {
 		"abecodes/tabout.nvim",
 		config = function()
 			require("tabout").setup({
-				tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
-				backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
-				act_as_tab = true, -- shift content if tab out is not possible
-				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-				default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-				default_shift_tab = "<C-d>", -- reverse shift default action,
-				enable_backwards = true, -- well ...
-				completion = false, -- if the tabkey is used in a completion pum
+				tabkey = "<Tab>",
+				backwards_tabkey = "<S-Tab>",
+				act_as_tab = true,
+				act_as_shift_tab = false,
+				default_tab = "<C-t>",
+				enable_backwards = true,
+				completion = false,
 				tabouts = {
 					{ open = "'", close = "'" },
 					{ open = '"', close = '"' },
@@ -77,11 +76,11 @@ return {
 					{ open = "[", close = "]" },
 					{ open = "{", close = "}" },
 				},
-				ignore_beginning = false, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-				exclude = {}, -- tabout will ignore these filetypes
+				ignore_beginning = false,
+				exclude = {},
 			})
 		end,
-		event = "InsertEnter", -- Set the event to 'InsertCharPre' for better compatibility
+		event = "InsertEnter",
 		priority = 1000,
 	},
 
