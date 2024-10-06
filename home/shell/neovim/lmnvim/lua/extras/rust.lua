d5d6c8b39cce5e51769227bd82542b8d7b80829b
lemonknife <lemonife@protonmail.com>
2024-10-05 21:16:11 -0400

    hack(nvim): possibly no improvements

--- home/shell/neovim/lmnvim/lua/extras/rust.lua
+++ home/shell/neovim/lmnvim/lua/extras/rust.lua
@@ -1,100 +1,100 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{
-				"Saecki/crates.nvim",
-				event = { "BufRead Cargo.toml" },
-				opts = {
-					completion = {
-						cmp = { enabled = true },
-					},
-				},
-			},
-		},
-		---@param opts cmp.ConfigSchema
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, { name = "crates" })
-		end,
-	},
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        opts = {
+          completion = {
+            cmp = { enabled = true },
+          },
+        },
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "crates" })
+    end,
+  },
 
-	{
-		"mrcjkb/rustaceanvim",
-		version = vim.fn.has("nvim-0.10.0") == 0 and "^4" or false,
-		ft = { "rust" },
-		opts = {
-			server = {
-				on_attach = function(_, bufnr)
-					vim.keymap.set("n", "<leader>cR", function()
-						vim.cmd.RustLsp("codeAction")
-					end, { desc = "Code Action (Rust)", buffer = bufnr })
-					--	vim.keymap.set("n", "<leader>dr", function()
-					--		vim.cmd.RustLsp("debuggables")
-					--	end, { desc = "Rust Debuggables", buffer = bufnr })
-				end,
-				default_settings = {
-					-- rust-analyzer language server configuration
-					["rust-analyzer"] = {
-						cargo = {
-							allFeatures = true,
-							loadOutDirsFromCheck = true,
-							buildScripts = {
-								enable = true,
-							},
-						},
-						-- Add clippy lints for Rust.
-						checkOnSave = true,
-						procMacro = {
-							enable = true,
-							ignored = {
-								["async-trait"] = { "async_trait" },
-								["napi-derive"] = { "napi" },
-								["async-recursion"] = { "async_recursion" },
-							},
-						},
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			vim.g.rustaceanvim =
-				require("configs.rust")(vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {}))
-			if vim.fn.executable("rust-analyzer") == 0 then
-				LazyVim.error(
-					"**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
-					{ title = "rustaceanvim" }
-				)
-			end
-		end,
-	},
+  {
+    "mrcjkb/rustaceanvim",
+    version = vim.fn.has("nvim-0.10.0") == 0 and "^4" or false,
+    ft = { "rust" },
+    event = "BufRead",
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action (Rust)", buffer = bufnr })
+          --	vim.keymap.set("n", "<leader>dr", function()
+          --		vim.cmd.RustLsp("debuggables")
+          --	end, { desc = "Rust Debuggables", buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = true,
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = require("configs.rust")(vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {}))
+      if vim.fn.executable("rust-analyzer") == 0 then
+        LazyVim.error(
+          "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
+          { title = "rustaceanvim" }
+        )
+      end
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				taplo = {
-					keys = {
-						{
-							"K",
-							function()
-								if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-									require("crates").show_popup()
-								else
-									vim.lsp.buf.hover()
-								end
-							end,
-							desc = "Show Crate Documentation",
-						},
-					},
-				},
-			},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        taplo = {
+          keys = {
+            {
+              "K",
+              function()
+                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+                  require("crates").show_popup()
+                else
+                  vim.lsp.buf.hover()
+                end
+              end,
+              desc = "Show Crate Documentation",
+            },
+          },
+        },
+      },
 
-			setup = {
-				rust_analyzer = function()
-					return true
-				end,
-			},
-		},
-	},
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+    },
+  },
 }
