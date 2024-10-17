c08ac3554286fde66b7ef772f2bfe385ec065b37
lemonknife <lemonife@protonmail.com>
2024-10-17 12:28:32 -0400

    feat(nvim): edit lualine

--- home/shell/neovim/lmnvim/init.lua
+++ home/shell/neovim/lmnvim/init.lua
@@ -1,12 +1,13 @@
 vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
 vim.g.mapleader = " "
+vim.opt.laststatus = 3 -- for bufferline
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-	local repo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,24 +16,24 @@ local lazy_options = require("options.lazy")
 
 -- load plugins
 require("lazy").setup({
-	{
-		"NvChad/NvChad",
-		lazy = true,
-		branch = "v2.5",
-	},
+  {
+    "NvChad/NvChad",
+    lazy = true,
+    branch = "v2.5",
+  },
 
-	{
-		"LazyVim/LazyVim",
-		lazy = true,
-	},
+  {
+    "LazyVim/LazyVim",
+    lazy = true,
+  },
 
-	{ import = "plugins" },
-	{ import = "extras" },
+  { import = "plugins" },
+  { import = "extras" },
 }, lazy_options)
 
 require("options")
 require("autocmds")
 
 vim.schedule(function()
-	require("mappings")
+  require("mappings")
 end)
