d6c1cae2f5dc1e0aa63fd6873cce9cd867f23153
lemonknife <lemonife@protonmail.com>
2024-10-25 23:56:57 -0400

    feat(nvim): add todo comment

--- home/shell/neovim/lmnvim/lua/plugins/ui.lua
+++ home/shell/neovim/lmnvim/lua/plugins/ui.lua
@@ -341,4 +341,28 @@ return {
       require("noice").setup(opts)
     end,
   },
+
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = "LazyFile",
+    opts = {},
+    keys = {
+      {
+        "]t",
+        function()
+          require("todo-comments").jump_next()
+        end,
+        desc = "Next Todo Comment",
+      },
+      {
+        "[t",
+        function()
+          require("todo-comments").jump_prev()
+        end,
+        desc = "Previous Todo Comment",
+      },
+      { "<leader>xt", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    },
+  },
 }
